e463b7c425053ea4b01ac188567fb9fa924e5240
fedeholc <federicoholc@gmail.com>
2023-06-11 08:02:04 -0300

    update css

--- app/page.js
+++ app/page.js
@@ -9,15 +9,15 @@ export default function Home() {
         <h2>¿A cuál estás buscando?</h2>
 
         <div className="grid grid-cols-2 gap-10 max-w-800px mt-10">
-          <div className="link__insta w-96 border flex flex-col items-center">
+          <div className="borde_amarillo w-96 border flex flex-col items-center">
             <Image
               className="fotosperfil rounded-full border-2 border-black mt-4 mb-1"
               src="/ff3.jpg"
               width="150"
               height="150"
             />
-            <h3 className="link__pink">Federico Holc </h3>
-            <h3 className="link__pink">fotógrafo</h3>
+            <h3 className="subrayado">Federico Holc </h3>
+            <h3 className="subrayado">fotógrafo</h3>
 
             <p className="mt-6 mb-4">
               ↳
@@ -26,7 +26,7 @@ export default function Home() {
               </a>
             </p>
           </div>
-          <div className="link__insta w-96 border flex flex-col items-center">
+          <div className="borde_amarillo w-96 border flex flex-col items-center">
             <Image
               className="fotosperfil rounded-full border-2 border-black mt-4 mb-1"
               src="/fp.jpg"
@@ -34,8 +34,8 @@ export default function Home() {
               height="150"
             />
 
-            <h3 className="link__pink">Federico Holc</h3>
-            <h3 className="link__pink">programador</h3>
+            <h3 className="subrayado">Federico Holc </h3>
+            <h3 className="subrayado">fotógrafo</h3>
 
             <p className="mt-6 mb-4">
               ↳
